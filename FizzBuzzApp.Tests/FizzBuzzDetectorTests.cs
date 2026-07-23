74bea99363f2bd98c4d5991b450bba40b3c4dddc
Abdulrahman0sama <oabdo6015@gmail.com>
2026-07-24 02:47:49 +0300

    Add project files.

---
+++ FizzBuzzApp.Tests/FizzBuzzDetectorTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzApp.Tests
+{
+    public class FizzBuzzDetectorTests
+    {
+        [Fact]
+        public void ThirdWord_ShouldBeFizz()
+        {
+            var detector = new FizzBuzzDetector();
+
+            var result = detector.GetOverlappings("one two three");
+
+            Assert.Equal("one two Fizz", result.OutputString);
+            Assert.Equal(1, result.Count);
+        }
+
+
+        [Fact]
+        public void FifthWord_ShouldBeBuzz()
+        {
+            var detector = new FizzBuzzDetector();
+
+            var result = detector.GetOverlappings("one two three four five");
+
+            Assert.Equal("one two Fizz four Buzz", result.OutputString);
+            Assert.Equal(2, result.Count);
+        }
+
+        [Fact]
+        public void FifteenthWord_ShouldBeFizzBuzz()
+        {
+            var detector = new FizzBuzzDetector();
+
+            var input =
+                "one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen";
+
+            var result = detector.GetOverlappings(input);
+
+            Assert.Contains("FizzBuzz", result.OutputString);
+        }
+
+        [Fact]
+        public void EmptyString_ShouldReturnEmptyResult()
+        {
+            var detector = new FizzBuzzDetector();
+
+            var result = detector.GetOverlappings("");
+
+            Assert.Equal("", result.OutputString);
+            Assert.Equal(0, result.Count);
+        }
+
+        [Fact]
+        public void NullInput_ShouldThrowException()
+        {
+            var detector = new FizzBuzzDetector();
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                detector.GetOverlappings(null);
+            });
+        }
+
+    }
+}
---
+++ FizzBuzzApp_/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var detector = new FizzBuzzDetector();
+
+            var result = detector.GetOverlappings("Mary had a little lamb");
+
+            Console.WriteLine(result.OutputString);
+            Console.WriteLine(result.Count);
+        }
+    }
+}
